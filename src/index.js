b6f1669587c17a804a83d2f86b16c4a3eb299421
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-03-14 01:34:40 +0600

    Add api key and other informations

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const containerForm = document.querySelector('[container-form]');
   let input = 'Dhaka';
   let searchTerm;
 
+getLocalTime = (data) => {
+    let date = new Date();
+    let time = date.getTime();
+    let localOffset = date.getTimezoneOffset() * 60000;
+    let utc = time + localOffset;
+    let localTime = utc + 1000 * data;
+    let localTimeDate = new Date(localTime);
+    return localTimeDate.toLocaleString();
+  }
+
 kelvinToFahrenheit = (temp) => {
   temp = parseFloat(temp);
   temp = Math.round(((temp = temp - 273.15) * 9) / 5 + 32);
@@ -34,7 +44,117 @@ clear = () => {
 }
 
 locButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log("hello");
+  e.preventDefault();
+  console.log("hello");
 })
 
+isANumber = (input) => {
+  return !/\D/.test(input);
+}
+
+containsNumber = (input) => {
+  return /\d/.test(input);
+}
+
+typeOfQuery = (input) => {
+  if(!isNaN(input) && containsNumber(input)) {
+    searchTerm = input;
+  } else if (isANumber(input) && input.length <= 5) {
+    searchTerm = `${'zip='}${input}`;
+  } else {
+    searchTerm = `${'q='}${input}`;
+  }
+}
+
+async function getWeather(input) {
+  typeOfQuery(input);
+  try {
+    const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?${searchTerm}&appid=49257f6591cfc3ed8daf0b5970d519cb&units=standard`,
+    { mode: 'cors' }
+    );
+    const data = await response.json();
+    displayWeather(data);
+  } catch (err) {
+      alert(err);
+    }
+}
+
+displayWeather = (data) => {
+  switch (data.weather[0].main) {
+    case 'Clear':
+      document.body.style.backgroundImage = 'url("./images/clear.jpg")';
+      break;
+    case 'Clouds':
+      document.body.style.backgroundImage = 'url("./images/cloudy.jpg")';
+      break;
+    case 'Rain':
+    case 'Drizzle':
+    case 'Mist':
+      document.body.style.backgroundImage ='url("./images/rain.jpg")';
+      break;
+    case 'Thunderstrom':
+      document.body.style.backgroundImage = 'url("./images/thunderstrom.jpg")';
+      break;
+    case 'Snow':
+      document.body.style.backgroundImage = 'url("./images/snow.jpg")';
+      break;
+    default:
+      break;
+  }
+
+  const cityName = document.querySelector('[cityName]');
+  const cityTemperature =document.querySelector('[cityTemperature]');
+  const cityWeatherDescription = document.querySelector('[cityWeather]');
+  const tempFeeling = document.querySelector('[tempFeeling]');
+  const humidityMeter = document.querySelector('[humidity]');
+  const windMeter = document.querySelector('[wind]');
+  const weatherImg = document.getElementById('image');
+  const datePreview = document.querySelector('[date-time]');
+  const dateAndTime = data.timezone;
+  const weatherDescription = data.weather[0].description;
+  let temp = data.main.temp;
+  let tempFeel = data.main.feels_like;
+
+  cityName.innerHTML = `${data.name}, ${data.sys.country}`;
+  datePreview.innerHTML = getLocalTime(dateAndTime);
+
+  if (toggleSwitch.checked) {
+    temp = kelvinToFahrenheit(temp);
+    tempFeel = kelvinToFahrenheit(tempFeel);
+    cityTemperature.innerHTML = `${temp + '&degF'}`;
+  } else {
+    temp = kelvinToCelcius(temp);
+    tempFeel = kelvinToCelcius(tempFeel);
+    cityTemperature.innerHTML = `${temp + '&degC'}`;
+  }
+
+  weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+  cityWeatherDescription.innerHTML =
+      weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+  tempFeeling.innerHTML = `${'Feels like: '}${tempFeel + '&deg'}`;
+
+  humidityMeter.innerHTML = `${'Humidity levels: '}${
+        data.main.humidity + '%'}`;
+  windMeter.innerHTML = `${'Wind: '}${data.wind.speed + 'k/m'}`;
+
+  toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+      setTimeout(() => {
+        temp = toFahrenheit(temp);
+        tempFeel = toFahrenheit(tempFeel);
+        cityTemperature.innerHTML = `${temp + '&degF'}`;
+        tempFeeling.innerHTML = `${'Feels like: '}${tempFeel + '&deg'}`;
+      }, 150);
+    } else {
+      setTimeout(() => {
+        temp = toCelsius(temp);
+        tempFeel = toCelsius(tempFeel);
+        cityTemperature.innerHTML = `${temp + '&degC'}`;
+        tempFeeling.innerHTML = `${'Feels like: '}${tempFeel + '&deg'}`;
+      }, 150);
+    }
+  });
+}
+getWeather(input);
